4f9034f61cb46200956722609e8c0b18aa505980
ddashadar <dashadar2003@gmail.com>
2021-10-09 02:20:26 +0300

    program

---
+++ 1.py
@@ -0,0 +1,19 @@
+try:
+    n = int(input('Введите количество элеметов - '))
+except ValueError:
+    print ('Ошибка')
+else:
+    array = [0]*n
+    try:
+        for i in range(0,n):
+            array[i] = int(input('Введите элемент - '))
+    except ValueError:
+        print ('Ошибка')
+    else:
+        try:
+            delta = int(input('Введите число - '))
+        except ValueError:
+            print ('Ошибка')
+        else:
+            result1 = array.count(min(array) + delta)
+            print('Количество элементов - ', result1)
